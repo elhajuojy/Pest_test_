5a33ce9403df805a93a8281f8814723586a0ef73
elhajuojy <souirimehdi311@gmail.com>
2023-02-10 10:48:50 +0100

    array equal Pest 😍

--- tests/Feature/PracticeTest.php
+++ tests/Feature/PracticeTest.php
@@ -1,10 +1,16 @@
  <?php
- it('asserts true is true', function () {
-//     $this->assertTrue(true);
+// it('asserts true is true', function () {
+////     $this->assertTrue(true);
+//
+//     expect(true)->toBeTrue();
+// });
 
-     expect(true)->toBeTrue();
- });
+it("is equal",function (){
 
+    $array = [1, 2, 3, 4];
 
+    $this->assertEquals([1, 2, 3, 4], $array);
+
+});
 
 //todo run this command :  ./vendor/bin/pest Tests/Feature/PracticeTest.php
