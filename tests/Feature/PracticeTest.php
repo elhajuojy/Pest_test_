9fc1decae91bf216bab923349164b0825fbc991e
elhajuojy <souirimehdi311@gmail.com>
2023-02-10 10:38:36 +0100

    Initial commit

---
+++ tests/Feature/PracticeTest.php
@@ -0,0 +1,10 @@
+ <?php
+ it('asserts true is true', function () {
+//     $this->assertTrue(true);
+
+     expect(true)->toBeTrue();
+ });
+
+
+
+//todo run this command :  ./vendor/bin/pest Tests/Feature/PracticeTest.php
